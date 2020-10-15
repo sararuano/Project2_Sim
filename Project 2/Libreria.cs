f6fef7a8fb17d3da07172f167cc0e0085329abb3
gerar <gerar@ORDENADOR-DE-GE>
2020-10-15 16:15:35 +0200

    prueba 5

--- Project 2/Libreria.cs
+++ Project 2/Libreria.cs
@@ -17,6 +17,7 @@ public class Class1
 		Console.WriteLine(variable1.ToString()));
 		Console.WriteLine("Diego no hce una mierda");
 		Console.WriteLine("Diego no hce una mierda1");
+		int variable2;
 
 	}
 }
